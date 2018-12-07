8cf82dbb7e2f34d8435b85bfd074e587f15d889f
wuguangyao <674147537@qq.com>
2018-12-07 16:58:23 +0800

    Future测试

---
+++ src/com/wuguangyao/test/demo/Demo.java
@@ -0,0 +1,9 @@
+package com.wuguangyao.test.demo;
+
+/**
+ * @author guangyao.wu
+ * @description TODO
+ * @date 2018/12/7 16:28
+ **/
+public class Demo {
+}
---
+++ src/com/wuguangyao/test/thread/WGYFutureTest.java
@@ -0,0 +1,42 @@
+package com.wuguangyao.test.thread;
+
+import java.util.concurrent.*;
+
+/**
+ * @author guangyao.wu
+ * @description TODO
+ * @date 2018/12/7 16:27
+ **/
+public class WGYFutureTest {
+
+
+    private static ThreadPoolExecutor executor = new ThreadPoolExecutor(10,
+            15,5, TimeUnit.MINUTES,new LinkedBlockingDeque<>());
+
+
+    static class CallableTask implements Callable<Integer>{
+
+        @Override
+        public Integer call() throws Exception {
+            return Integer.MIN_VALUE;
+        }
+    }
+
+    static class RunnableTask implements Runnable{
+
+        @Override
+        public void run() {
+            System.out.println("WGYFutureTest");
+        }
+    }
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        Future<?> future = executor.submit(new RunnableTask());
+        Object o = future.get();
+        System.out.println(o);
+        Future<Integer> integerFuture = executor.submit(new CallableTask());
+        Integer integer = integerFuture.get();
+        System.out.println(integer);
+    }
+
+}
